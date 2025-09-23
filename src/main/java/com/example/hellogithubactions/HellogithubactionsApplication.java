1c0420c7bfb07e017101d16b90a35e49476b93de
Anh Dang Thi Hoang <anh.dangthihoang@emeal.nttdata.com>
2025-09-23 14:45:21 +0700

    Initial commit

---
+++ src/main/java/com/example/hellogithubactions/HellogithubactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellogithubactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellogithubactionsApplication.class, args);
+	}
+
+}
